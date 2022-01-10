6c3e11fa947c51f77f3f8c8cbff7f4144a37dfc4
Dayan C. Galiazzi <galiazzi.dayan@gmail.com>
2022-01-10 18:17:42 -0300

    feat: add stdin fmt

--- deps.ts
+++ deps.ts
@@ -1,6 +1,8 @@
-export { posix } from "https://deno.land/std@0.119.0/path/mod.ts";
-export { pooledMap } from "https://deno.land/std@0.119.0/async/pool.ts";
-export { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
+export { posix } from "https://deno.land/std@0.120.0/path/mod.ts";
+export { pooledMap } from "https://deno.land/std@0.120.0/async/pool.ts";
+export { parse } from "https://deno.land/std@0.120.0/flags/mod.ts";
 
 export { diffLines } from "https://cdn.skypack.dev/diff";
-export { gray, green, red } from "https://deno.land/std@0.119.0/fmt/colors.ts";
+export { gray, green, red } from "https://deno.land/std@0.120.0/fmt/colors.ts";
+
+export { readAllSync } from "https://deno.land/std@v0.120.0/streams/conversion.ts";
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { parse, pooledMap } from "./deps.ts";
-import { check, fmt } from "./format.ts";
+import { parse, pooledMap, readAllSync } from "./deps.ts";
+import { check, fmt, formatSource } from "./format.ts";
 import { lint } from "./lint.ts";
 import { getFiles } from "./util.ts";
 
@@ -18,6 +18,16 @@ if (!["fmt", "lint"].includes(cmd as string)) {
     `\nInvalid command ${cmd}.\n\nValid commands are fmt or lint\n`,
   );
   Deno.exit(1);
+}
+
+const isStdin = argv._.shift() === "-" && cmd == "fmt";
+if (isStdin) {
+  const formated = await formatSource(
+    new TextDecoder().decode(readAllSync(Deno.stdin)),
+    argv.check,
+  );
+  Deno.stdout.writeSync(new TextEncoder().encode(formated));
+  Deno.exit(0);
 }
 
 const currentJobs = 5;
