9ba649e17a421a58fd7e0e0438c9862841a1d5c7
Dayan C. Galiazzi <galiazzi.dayan@gmail.com>
2022-01-13 10:54:27 -0300

    fix: import std streams link

--- src/deps.ts
+++ src/deps.ts
@@ -5,4 +5,4 @@ export { parse } from "https://deno.land/std@0.120.0/flags/mod.ts";
 export { diffLines } from "https://cdn.skypack.dev/diff";
 export { gray, green, red } from "https://deno.land/std@0.120.0/fmt/colors.ts";
 
-export { readAllSync } from "https://deno.land/std@v0.120.0/streams/conversion.ts";
+export { readAllSync } from "https://deno.land/std@0.120.0/streams/conversion.ts";
